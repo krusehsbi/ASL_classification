306afce4d9b402b3628968be94c46e88b6776cbf
Alexander Kruse <krusealexander00@gmail.com>
2024-04-29 17:24:23 +0200

    changes in datastructure

--- dataset_inspect.py
+++ dataset_inspect.py
@@ -2,15 +2,17 @@ import cv2
 import os
 import random
 
-folder = "data/asl_alphabet_ds/asl_alphabet_train"
+
+folder = os.path.join(os.path.dirname(__file__), 'data/syntetic_asl_dataset/Test_Alphabet')
 
 for letter in os.listdir(folder):
     folder_letter = os.path.join(folder, letter)
     all_letters = os.listdir(folder_letter)
     for image in random.sample(all_letters,1):
         img = os.path.join(folder_letter, image)
+
         im = cv2.imread(img)
         cv2.imshow(letter, im)
-        cv2.waitKey(0) 
+        cv2.waitKey(0)
   
 cv2.destroyAllWindows()
---
+++ dataset_rename.py
@@ -0,0 +1,27 @@
+import cv2
+import os
+import random
+
+def rename_set(folder):
+
+    for letter in os.listdir(folder):
+        
+        folder_letter = os.path.join(folder, letter)
+        print('Renaming Images of Letter ' + letter)
+        i=0
+        for image in os.listdir(folder_letter):
+            
+            renamed_img = os.path.join(folder_letter, '{}_{}.jpg'.format(letter, i))
+            img = os.path.join(folder_letter, image)
+            
+            os.rename(img, renamed_img)
+            i+=1
+            
+
+folder_train = os.path.join(os.path.dirname(__file__), 'data/syntetic_asl_dataset/Train_Alphabet')
+folder_test = os.path.join(os.path.dirname(__file__), 'data/syntetic_asl_dataset/Test_Alphabet')
+
+print('RENAMING TRAIN-SET...')
+rename_set(folder_train)
+print('RENAMING TEST-SET...')
+rename_set(folder_test)
